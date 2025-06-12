12a0a8ba58bf2352deb10dfc49103e6d405114b2
thanhdi721 <thanhdi721@gmail.com>
2025-06-12 22:26:43 +0700

    update

--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,163 +1,178 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   View,
   Text,
   StyleSheet,
+  Image,
   ScrollView,
   TouchableOpacity,
-} from 'react-native';
+} from "react-native";
 
 const passengerData = [
   {
-    from: '129 Phạm Ngọc Thạch, Đống Đa',
-    to: '77 Hào Nam, Ô Chợ Dừa, Đống Đa,...',
-    time: '14:50',
-    date: '21/2/2025',
-    price: '20.000',
+    from: "129 Phạm Ngọc Thạch, Đống Đa",
+    to: "77 Hào Nam, Ô Chợ Dừa, Đống Đa,...",
+    time: "14:50",
+    date: "21/2/2025",
+    price: "20.000",
   },
   {
-    from: 'Ngõ 38, Đan Phượng, Phượng Trì,...',
-    to: 'Số 6 Đào Duy Anh, Hà Nội',
-    time: '14:50',
-    date: '21/2/2025',
-    price: '35.000',
+    from: "Ngõ 38, Đan Phượng, Phượng Trì,...",
+    to: "Số 6 Đào Duy Anh, Hà Nội",
+    time: "14:50",
+    date: "21/2/2025",
+    price: "35.000",
   },
   {
-    from: '10 Võ Văn Kiệt, TP. Hồ Chí Minh',
-    to: 'Đại học FPT Khu Công Nghệ Cao',
-    time: '14:50',
-    date: '21/2/2025',
-    price: '120.000',
+    from: "10 Võ Văn Kiệt, TP. Hồ Chí Minh",
+    to: "Đại học FPT Khu Công Nghệ Cao",
+    time: "14:50",
+    date: "21/2/2025",
+    price: "120.000",
   },
   {
-    from: '8 Trần Hưng Đạo, Đà Lạt',
-    to: '55 Hoàng Hoa Thám, Đà Lạt',
-    time: '14:50',
-    date: '21/2/2025',
-    price: '97.000',
+    from: "8 Trần Hưng Đạo, Đà Lạt",
+    to: "55 Hoàng Hoa Thám, Đà Lạt",
+    time: "14:50",
+    date: "21/2/2025",
+    price: "97.000",
   },
   {
-    from: '15 Lý Tự Trọng, Nha Trang',
-    to: '77 Nguyễn Văn Cừ, Nha Trang',
-    time: '14:50',
-    date: '21/2/2025',
-    price: '89.000',
+    from: "15 Lý Tự Trọng, Nha Trang",
+    to: "77 Nguyễn Văn Cừ, Nha Trang",
+    time: "14:50",
+    date: "21/2/2025",
+    price: "89.000",
   },
   {
-    from: 'Số 26 Quang Trung, Hoàn Kiếm',
-    to: 'Số 17 Lý Nam Đế, Hoàn Kiếm',
-    time: '14:50',
-    date: '21/2/2025',
-    price: '78.000',
+    from: "Số 26 Quang Trung, Hoàn Kiếm",
+    to: "Số 17 Lý Nam Đế, Hoàn Kiếm",
+    time: "14:50",
+    date: "21/2/2025",
+    price: "78.000",
   },
   {
-    from: 'Số 10 Cầu Gỗ, Hoàn Kiếm',
-    to: 'Số 29 Lý Thường Kiệt, Hoàn Kiếm',
-    time: '14:50',
-    date: '21/2/2025',
-    price: '65.000',
+    from: "Số 10 Cầu Gỗ, Hoàn Kiếm",
+    to: "Số 29 Lý Thường Kiệt, Hoàn Kiếm",
+    time: "14:50",
+    date: "21/2/2025",
+    price: "65.000",
   },
   {
-    from: 'Số 15 Trần Quang Khải, Hoàn Kiếm',
-    to: 'Số 31 Nguyễn Thái Học, Ba Đình',
-    time: '14:50',
-    date: '21/2/2025',
-    price: '87.000',
+    from: "Số 15 Trần Quang Khải, Hoàn Kiếm",
+    to: "Số 31 Nguyễn Thái Học, Ba Đình",
+    time: "14:50",
+    date: "21/2/2025",
+    price: "87.000",
   },
   {
-    from: 'Số 88 Tràng Tiền, Hoàn Kiếm',
-    to: 'Số 88 Tràng Tiền, Hoàn Kiếm',
-    time: '14:50',
-    date: '21/2/2025',
-    price: '35.000',
+    from: "Số 88 Tràng Tiền, Hoàn Kiếm",
+    to: "Số 88 Tràng Tiền, Hoàn Kiếm",
+    time: "14:50",
+    date: "21/2/2025",
+    price: "35.000",
   },
 ];
 
 const driverData = [
   {
-    from: '129 Phạm Ngọc Thạch, Đống Đa',
-    to: '77 Hào Nam, Ô Chợ Dừa, Đống Đa,...',
-    time: '14:50',
-    date: '21/2/2025',
-    price: '12.000',
+    from: "129 Phạm Ngọc Thạch, Đống Đa",
+    to: "77 Hào Nam, Ô Chợ Dừa, Đống Đa,...",
+    time: "14:50",
+    date: "21/2/2025",
+    price: "12.000",
   },
   {
-    from: 'Ngõ 38, Đan Phượng, Phượng Trì,...',
-    to: 'Số 6 Đào Duy Anh, Hà Nội',
-    time: '14:50',
-    date: '21/2/2025',
-    price: '29.000',
+    from: "Ngõ 38, Đan Phượng, Phượng Trì,...",
+    to: "Số 6 Đào Duy Anh, Hà Nội",
+    time: "14:50",
+    date: "21/2/2025",
+    price: "29.000",
   },
   {
-    from: '10 Võ Văn Kiệt, TP. Hồ Chí Minh',
-    to: 'Đại học FPT Khu Công Nghệ Cao',
-    time: '14:50',
-    date: '21/2/2025',
-    price: '98.000',
+    from: "10 Võ Văn Kiệt, TP. Hồ Chí Minh",
+    to: "Đại học FPT Khu Công Nghệ Cao",
+    time: "14:50",
+    date: "21/2/2025",
+    price: "98.000",
   },
   {
-    from: '8 Trần Hưng Đạo, Đà Lạt',
-    to: '55 Hoàng Hoa Thám, Đà Lạt',
-    time: '14:50',
-    date: '21/2/2025',
-    price: '75.000',
+    from: "8 Trần Hưng Đạo, Đà Lạt",
+    to: "55 Hoàng Hoa Thám, Đà Lạt",
+    time: "14:50",
+    date: "21/2/2025",
+    price: "75.000",
   },
   {
-    from: '15 Lý Tự Trọng, Nha Trang',
-    to: '77 Nguyễn Văn Cừ, Nha Trang',
-    time: '14:50',
-    date: '21/2/2025',
-    price: '77.000',
+    from: "15 Lý Tự Trọng, Nha Trang",
+    to: "77 Nguyễn Văn Cừ, Nha Trang",
+    time: "14:50",
+    date: "21/2/2025",
+    price: "77.000",
   },
   {
-    from: 'Số 26 Quang Trung, Hoàn Kiếm',
-    to: 'Số 17 Lý Nam Đế, Hoàn Kiếm',
-    time: '14:50',
-    date: '21/2/2025',
-    price: '76.000',
+    from: "Số 26 Quang Trung, Hoàn Kiếm",
+    to: "Số 17 Lý Nam Đế, Hoàn Kiếm",
+    time: "14:50",
+    date: "21/2/2025",
+    price: "76.000",
   },
   {
-    from: 'Số 10 Cầu Gỗ, Hoàn Kiếm',
-    to: 'Số 29 Lý Thường Kiệt, Hoàn Kiếm',
-    time: '14:50',
-    date: '21/2/2025',
-    price: '54.000',
+    from: "Số 10 Cầu Gỗ, Hoàn Kiếm",
+    to: "Số 29 Lý Thường Kiệt, Hoàn Kiếm",
+    time: "14:50",
+    date: "21/2/2025",
+    price: "54.000",
   },
   {
-    from: 'Số 15 Trần Quang Khải, Hoàn Kiếm',
-    to: 'Số 31 Nguyễn Thái Học, Ba Đình',
-    time: '14:50',
-    date: '21/2/2025',
-    price: '64.000',
+    from: "Số 15 Trần Quang Khải, Hoàn Kiếm",
+    to: "Số 31 Nguyễn Thái Học, Ba Đình",
+    time: "14:50",
+    date: "21/2/2025",
+    price: "64.000",
   },
   {
-    from: 'Số 88 Tràng Tiền, Hoàn Kiếm',
-    to: 'Số 88 Tràng Tiền, Hoàn Kiếm',
-    time: '14:50',
-    date: '21/2/2025',
-    price: '23.000',
+    from: "Số 88 Tràng Tiền, Hoàn Kiếm",
+    to: "Số 88 Tràng Tiền, Hoàn Kiếm",
+    time: "14:50",
+    date: "21/2/2025",
+    price: "23.000",
   },
 ];
 
 const HistoryScreen = () => {
-    const [tab, setTab] = useState('hanhkhach'); 
+  const [tab, setTab] = useState("hanhkhach");
 
-    const renderList = (dataList) => (
+  const renderList = (dataList) => (
     <ScrollView style={styles.list}>
       {dataList.map((item, index) => (
         <View key={index} style={styles.card}>
-          <Text style={styles.place}>{item.from}</Text>
-          <View style={styles.row}>
-            <Text style={styles.arrow}>↓</Text>
-            <Text style={styles.place}>{item.to}</Text>
-          </View>
-          <View style={styles.footerRow}>
-            <View>
-              <Text style={styles.subInfo}>{item.time}</Text>
-              <Text style={styles.subInfo}>{item.date}</Text>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <View style={{ flexDirection: "row", flex: 1 }}>
+              <Image
+                source={require("../assets/ic-doc.png")}
+                style={styles.dot}
+              />
+              <View style={styles.row}>
+                <Text style={styles.place}>{item.from}</Text>
+                <Text style={styles.place}>{item.to}</Text>
+              </View>
             </View>
+            <Text style={styles.price}>{item.price}</Text>
+          </View>
+
+          <View style={styles.footerRow}>
+            <Text style={styles.subInfo}>
+              {item.time} {item.date}
+            </Text>
+
             <View style={styles.rightActions}>
-              <Text style={styles.price}>{item.price}</Text>
               <TouchableOpacity>
                 <Text style={styles.reorder}>Đặt lại →</Text>
               </TouchableOpacity>
@@ -173,99 +188,106 @@ const HistoryScreen = () => {
       {/* Tabs */}
       <View style={styles.tabs}>
         <TouchableOpacity
-          style={[styles.tab, tab === 'hanhkhach' && styles.activeTab]}
-          onPress={() => setTab('hanhkhach')}>
+          style={[styles.tab, tab === "hanhkhach" && styles.activeTab]}
+          onPress={() => setTab("hanhkhach")}
+        >
           <Text
-            style={[styles.tabText, tab === 'hanhkhach' && styles.activeTabText]}>
+            style={[
+              styles.tabText,
+              tab === "hanhkhach" && styles.activeTabText,
+            ]}
+          >
             Hành khách
           </Text>
         </TouchableOpacity>
         <TouchableOpacity
-          style={[styles.tab, tab === 'taixe' && styles.activeTab]}
-          onPress={() => setTab('taixe')}>
-          <Text style={[styles.tabText, tab === 'taixe' && styles.activeTabText]}>
+          style={[styles.tab, tab === "taixe" && styles.activeTab]}
+          onPress={() => setTab("taixe")}
+        >
+          <Text
+            style={[styles.tabText, tab === "taixe" && styles.activeTabText]}
+          >
             Tài xế
           </Text>
         </TouchableOpacity>
       </View>
 
-      {tab === 'hanhkhach' ? renderList(passengerData) : renderList(driverData)}
+      {tab === "hanhkhach" ? renderList(passengerData) : renderList(driverData)}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#fff' },
+  container: { flex: 1, backgroundColor: "#fff" },
   tabs: {
-    flexDirection: 'row',
-    backgroundColor: '#F2F2F2',
+    flexDirection: "row",
+    backgroundColor: "#F2F2F2",
     margin: 16,
     borderRadius: 12,
   },
   tab: {
     flex: 1,
     paddingVertical: 12,
-    alignItems: 'center',
+    alignItems: "center",
     borderRadius: 12,
   },
   activeTab: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     elevation: 2,
   },
   tabText: {
     fontSize: 14,
-    color: '#777',
+    color: "#777",
   },
   activeTabText: {
-    color: '#000',
-    fontWeight: '600',
+    color: "#000",
+    fontWeight: "600",
   },
   list: {
     paddingHorizontal: 16,
   },
   card: {
-    backgroundColor: '#F9F9F9',
+    backgroundColor: "#F9F9F9",
     padding: 12,
     borderRadius: 10,
     marginBottom: 12,
     borderWidth: 1,
-    borderColor: '#E0E0E0',
+    borderColor: "#E0E0E0",
   },
   row: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flex: 1,
   },
-  arrow: {
-    fontSize: 18,
-    marginRight: 4,
-    color: '#888',
+  dot: {
+    marginTop: 10,
+    marginRight: 10,
   },
   place: {
     fontSize: 14,
-    color: '#333',
+    color: "#333",
     marginBottom: 4,
   },
   footerRow: {
-    marginTop: 8,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    marginTop: 0,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   subInfo: {
     fontSize: 12,
-    color: '#777',
+    color: "#777",
   },
   rightActions: {
-    alignItems: 'flex-end',
+    alignItems: "flex-end",
   },
   price: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 14,
-    color: '#000',
+    color: "#000",
+    marginLeft: 10,
   },
   reorder: {
     fontSize: 12,
-    color: '#007BFF',
+    color: "#007BFF",
     marginTop: 4,
   },
 });
--- screens/NotificationsScreen.js
+++ screens/NotificationsScreen.js
@@ -1,10 +1,117 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+} from "react-native";
+
+const messages = [
+  {
+    name: "Trần Tiến Đạt",
+    message: "Bạn ơi mình đến rồi nhé",
+    time: "Th3 14:25",
+    unread: true,
+    avatar: "https://i.pravatar.cc/50?img=1",
+  },
+  {
+    name: "Phạm Hoàng Minh Khôi",
+    message: "Cảm ơn bạn nhiều nhé",
+    time: "Th3 14:25",
+    unread: false,
+    avatar: "https://i.pravatar.cc/50?img=2",
+  },
+  {
+    name: "Hứa Quang Hán",
+    message: "Bạn: Okeee",
+    time: "Th3 14:25",
+    unread: false,
+    avatar: "https://i.pravatar.cc/50?img=3",
+  },
+  {
+    name: "Bạch Kính Đình",
+    message: "ok nha",
+    time: "Th3 14:25",
+    unread: false,
+    avatar: "https://i.pravatar.cc/50?img=4",
+  },
+];
+
+const notifications = [
+  "Đã tìm thấy đối tác cho chuyến đi từ 129 Phạm Ngọc Thạch vào 14:50 21/2/2025",
+  "Đã tìm thấy đối tác cho chuyến đi từ 100 Nguyễn Kiệm vào 5:45 24/1/2025",
+  "Đã tìm thấy đối tác cho chuyến đi từ 23 Lương Định Của vào 6:20 24/1/2025",
+  "Đã tìm thấy đối tác cho chuyến đi từ 55 Trần Hưng Đạo vào 6:50 24/1/2025",
+  "Đã tìm thấy đối tác cho chuyến đi từ 32 Trương Định 7:20 24/1/2025",
+  "Đã tìm thấy đối tác cho chuyến đi từ 23 Hoàng Hoa Thám vào 7:40 24/1/2025",
+  "Đã tìm thấy đối tác cho chuyến đi từ Số 8 Nguyễn Xiển vào 8:12 24/1/2025",
+  "Đã tìm thấy đối tác cho chuyến đi từ 98 Lê Quang Định vào 9:20 24/1/2025",
+  "Đã tìm thấy đối tác cho chuyến đi từ 99 Phạm Ngũ Lão vào 17:20 24/1/2025",
+  "Đã tìm thấy đối tác cho chuyến đi từ Số 1 Phạm Văn Đồng vào 21:20 24/1/2025",
+  "Đã tìm thấy đối tác cho chuyến đi từ 100/5 Nguyễn Kiệm vào 22:20 24/1/2025",
+];
 
 const NotificationsScreen = () => {
+  const [tab, setTab] = useState("messages"); // 'messages' or 'notifications'
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Thông báo</Text>
+
+      {/* Tabs */}
+      <View style={styles.tabs}>
+        <TouchableOpacity
+          style={[styles.tab, tab === "messages" && styles.activeTab]}
+          onPress={() => setTab("messages")}
+        >
+          <Text
+            style={[styles.tabText, tab === "messages" && styles.activeTabText]}
+          >
+            Tin nhắn
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.tab, tab === "notifications" && styles.activeTab]}
+          onPress={() => setTab("notifications")}
+        >
+          <Text
+            style={[
+              styles.tabText,
+              tab === "notifications" && styles.activeTabText,
+            ]}
+          >
+            Thông báo
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Content */}
+      <ScrollView style={styles.contentBox}>
+        {tab === "messages" &&
+          messages.map((item, index) => (
+            <View key={index} style={styles.messageItem}>
+              <Image source={{ uri: item.avatar }} style={styles.avatar} />
+              <View style={{ flex: 1, marginLeft: 10 }}>
+                <Text style={styles.name}>{item.name}</Text>
+                <Text style={styles.message}>{item.message}</Text>
+              </View>
+              <View style={{ alignItems: "flex-end" }}>
+                <Text style={styles.time}>{item.time}</Text>
+                {item.unread && <View style={styles.dot} />}
+              </View>
+            </View>
+          ))}
+
+        {tab === "notifications" &&
+          notifications.map((text, index) => (
+            <View key={index} style={styles.notificationItem}>
+              <Text style={styles.notificationText}>{text}</Text>
+              <View style={styles.dotOverlay} />
+            </View>
+          ))}
+      </ScrollView>
     </View>
   );
 };
@@ -12,14 +119,97 @@ const NotificationsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
+    paddingHorizontal: 16,
   },
   title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 16,
+    fontSize: 22,
+    fontWeight: "700",
+    marginVertical: 16,
+  },
+  tabs: {
+    flexDirection: "row",
+    backgroundColor: "#f0f0f0",
+    borderRadius: 12,
+    marginBottom: 12,
+  },
+  tab: {
+    flex: 1,
+    paddingVertical: 12,
+    alignItems: "center",
+    borderRadius: 12,
+  },
+  activeTab: {
+    backgroundColor: "#fff",
+    elevation: 3,
+  },
+  tabText: {
+    fontSize: 14,
+    color: "#777",
+  },
+  activeTabText: {
+    color: "#000",
+    fontWeight: "600",
+  },
+  contentBox: {
+    flex: 1,
+    backgroundColor: "#f9f9f9",
+    borderRadius: 16,
+    padding: 12,
+  },
+  messageItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 12,
+    borderBottomWidth: 0.8,
+    borderColor: "#ddd",
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+  name: {
+    fontWeight: "600",
+    fontSize: 14,
+    marginBottom: 4,
+  },
+  message: {
+    color: "#444",
+    fontSize: 13,
+  },
+  time: {
+    fontSize: 12,
+    color: "#888",
+  },
+  dot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: "red",
+    marginTop: 4,
+    alignSelf: "flex-end",
+  },
+  notificationItem: {
+    marginBottom: 18,
+    position: "relative",
+    paddingRight: 16,
+  },
+  notificationText: {
+    fontSize: 13,
+    color: "#333",
+    lineHeight: 18,
+  },
+  dotOverlay: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: "red",
+    position: "absolute",
+    top: "50%",
+    right: 0,
+    marginTop: -4,
   },
 });
 
-export default NotificationsScreen; 
+export default NotificationsScreen;
